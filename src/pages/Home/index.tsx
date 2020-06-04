40d139716020ae7151f6ddc94d8fb115e230a168
Javel Queiroz <javelqueiroz@outlook.com.br>
2020-06-04 12:16:04 -0300

    Installing react navigation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { View, Image } from 'react-native';
-
-
+import { View, Image, ImageBackground, Text } from 'react-native';
+import styles from '../Home/styles';
+import {RectButton} from 'react-native-gesture-handler';
 
 const Home = () => {
     return (
-        <View>
-            <Image source={require('../../assets/logo.png')} />
-        </View>
+        <ImageBackground 
+        source={require('../../assets/home-background.png')} 
+        style={styles.container}
+        imageStyle={{width: 274, height: 278}}
+        >
+            <View style={styles.main}>
+                <Image source={require('../../assets/logo.png')} />
+                <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+                <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente</Text>
+            </View>
+            
+        </ImageBackground>
     );
 };
 
